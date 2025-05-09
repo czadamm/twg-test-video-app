987fe2a3ec92841a34ffe4f7ab55ac5bd1eb33de
adam <adam11392@gmail.com>
2025-05-09 17:41:04 +0200

    TVA-6 | added prettier

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,17 +1,17 @@
-import {Button, Text, View} from "react-native";
-import * as React from "react";
-import {NativeStackScreenProps} from "@react-navigation/native-stack";
-import {RootStackParamList} from "@/App";
+import { Button, Text, View } from 'react-native';
+import * as React from 'react';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '@/App';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Main'>;
 
 export default function MainScreen({ navigation }: Props) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Main Screen</Text>
-            <Button title={'Login'} onPress={()=> navigation.navigate('Login')} />
-            <Button title={'Search'} onPress={()=> navigation.navigate('Search')} />
-            <Button title={'VideoDetails'} onPress={()=> navigation.navigate('VideoDetails', {videoId: '001'})} />
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Main Screen</Text>
+      <Button title={'Login'} onPress={() => navigation.navigate('Login')} />
+      <Button title={'Search'} onPress={() => navigation.navigate('Search')} />
+      <Button title={'VideoDetails'} onPress={() => navigation.navigate('VideoDetails', { videoId: '001' })} />
+    </View>
+  );
 }
--- src/screens/VideoDetails.tsx
+++ src/screens/VideoDetails.tsx
@@ -1,16 +1,16 @@
-import {Text, View} from "react-native";
-import * as React from "react";
-import {NativeStackScreenProps} from "@react-navigation/native-stack";
-import {RootStackParamList} from "@/App";
+import { Text, View } from 'react-native';
+import * as React from 'react';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '@/App';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'VideoDetails'>;
 
 export default function VideoDetailsScreen({ route, navigation }: Props) {
-    const videoId = route.params.videoId;
+  const videoId = route.params.videoId;
 
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>{`Video: ${videoId}`}</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>{`Video: ${videoId}`}</Text>
+    </View>
+  );
 }
