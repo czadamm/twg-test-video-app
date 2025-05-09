750463bb2f56ceb07dae3f6c6b7675fddb2a2ac7
adam <adam11392@gmail.com>
2025-05-09 17:10:04 +0200

    TVA-5 | created empty necessary screens: Login, Main, Search, VideoDetails, Settings

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -3,12 +3,12 @@ import * as React from "react";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "@/App";
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type Props = NativeStackScreenProps<RootStackParamList, 'Search'>;
 
-export default function HomeScreen({ navigation }: Props) {
+export default function SearchScreen({ navigation }: Props) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>Search Screen</Text>
         </View>
     );
 }
---
+++ src/screens/Settings.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from "react-native";
+import * as React from "react";
+import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {RootStackParamList} from "@/App";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Settings'>;
+
+export default function SettingsScreen({ navigation }: Props) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Settings Screen</Text>
+        </View>
+    );
+}
